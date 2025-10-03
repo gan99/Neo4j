27ec3e6934c35307418f390839e04adad1b7802b
gan99 <42866729+gan99@users.noreply.github.com>
2025-10-03 11:43:29 -0500

    Update app.override.js

--- app.override.js
+++ app.override.js
@@ -1,1 +1,12 @@
-(ele) => ele.data(currentQuery.edge_caption_property) || ele.data('label') || ele.data('name') || ''
+ (ele) => {            // <-- starts at line ~279
+      const prop = currentQuery && currentQuery.edge_caption_property;
+      const getText = (val) => { /* normalizes string/number/boolean/array/object */ };
+      const v1 = prop ? ele.data(prop) : undefined;
+      const v2 = getText(v1);
+      if (v2) return v2;
+      const v3 = getText(ele.data('label'));
+      if (v3) return v3;
+      const v4 = getText(ele.data('name'));
+      if (v4) return v4;
+      return '';
+    }        
